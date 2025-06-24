b74466aaac2482fd675a5be78e280eedc2a361f8
boddu5672 <boddu5672@gmail.com>
2025-06-24 12:49:23 +0530

    Staged

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -19,7 +19,7 @@ const Header = props => {
           <img
               className="header-logo"
               alt="website logo"
-              src="https://sdmntpreastus.oaiusercontent.com/files/00000000-680c-61f9-a2c7-fe598dd7665f/raw?se=2025-06-22T22%3A46%3A40Z&sp=r&sv=2024-08-04&sr=b&scid=17f7c52d-6f03-586a-a08b-aee81c708ccd&skoid=02b7f7b5-29f8-416a-aeb6-99464748559d&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-06-21T23%3A04%3A30Z&ske=2025-06-22T23%3A04%3A30Z&sks=b&skv=2024-08-04&sig=XQVh3JRR18UK9CQEs3h/zUFckWJrnJDQ6HjfOswZIIE%3D"
+              src="\assets\logo.png"
               style={{ width: '70px', height: 'auto',borderRadius:"0px 20px 0px 20px" }}
             />
         </Link>
