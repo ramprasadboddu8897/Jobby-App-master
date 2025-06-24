3ac645a33fef99dbd035a6cc4309219735bbe7c3
boddu5672 <boddu5672@gmail.com>
2025-06-24 10:41:54 +0530

    Logo Change

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -130,7 +130,8 @@ class Register extends Component {
             <img
               className="website-logo"
               alt="website logo"
-              src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+              src="https://sdmntpreastus.oaiusercontent.com/files/00000000-680c-61f9-a2c7-fe598dd7665f/raw?se=2025-06-22T22%3A46%3A40Z&sp=r&sv=2024-08-04&sr=b&scid=17f7c52d-6f03-586a-a08b-aee81c708ccd&skoid=02b7f7b5-29f8-416a-aeb6-99464748559d&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-06-21T23%3A04%3A30Z&ske=2025-06-22T23%3A04%3A30Z&sks=b&skv=2024-08-04&sig=XQVh3JRR18UK9CQEs3h/zUFckWJrnJDQ6HjfOswZIIE%3D"
+              style={{ width: '70px', height: '80',borderRadius:"0px 20px 0px 20px" }}
             />
             {this.renderInputField('Name', 'text', 'name', name, this.onChangeInput('name'))}
             {this.renderInputField('Email', 'email', 'email', email, this.onChangeInput('email'))}
